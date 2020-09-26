a8ed590fa7a77286f109d679a376591e0f9ea881
Jag Nanda <jnanda313@gmail.com>
2020-09-25 23:38:50 -0400

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,142 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:quizzler/QuizLogic.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+void main() => runApp(Quiz());
+
+class Quiz extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  QuizLogic quizLogic = new QuizLogic();
+
+  void checkAnswer(bool answerSelected) {
+    bool isUserCorrect = quizLogic.checkAnswer(answerSelected);
+    print(quizLogic.getQuestionsRemainingCount());
+    if (quizLogic.getQuestionsRemainingCount() > 0) {
+      setState(() {
+        if (isUserCorrect) {
+          scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+        } else {
+          scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+        }
+        quizLogic.generateNextRandomQuestion();
+      });
+    } else {
+      //End of quiz
+      Alert(
+        context: context,
+        title: "Reached End of Quiz",
+        desc: "Press restart to restart the quiz",
+        buttons: [
+          DialogButton(
+            child: Text("Restart"),
+            onPressed: () {
+              quizLogic = new QuizLogic();
+              setState(() {
+                scoreKeeper = [];
+              });
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ).show();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizLogic.getRandomQuestion(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+                checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
